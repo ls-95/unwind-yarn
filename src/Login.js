99f7dd5a47f16eede2a90e43fc0b9e269ed2d5b7
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-07-18 12:54:11 +0200

    Created login page

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,50 @@
+import "./Login.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email, password);
+    navigate("/home");
+  }
+
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePassword(event) {
+    setPassword(event.target.value);
+  }
+
   return (
     <div className="Login">
-      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="LoginContent">
+          <h1>Login</h1>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            onChange={handleEmail}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={handlePassword}
+          />
+          <input type="submit" value="Login" className="LoginButton" />
+          <p>
+            Don't have an account? <Link to="/">Signup</Link>
+          </p>
+        </div>
+      </form>
     </div>
   );
 }
