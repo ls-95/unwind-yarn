35d06146d6c500d3fbcb5c3e56e3be3843141d70
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-07-24 10:42:30 +0200

    Updated YarnCompositionForm.js

--- src/pages/yarn/YarnCompositionForm.js
+++ src/pages/yarn/YarnCompositionForm.js
@@ -10,7 +10,7 @@ export default function YarnCompositionForm({ registerField }) {
         </div>
         <div className="FormInputText">
           <input
-            {...registerField("composition.materialone")}
+            {...registerField("composition.material")}
             type="text"
             placeholder="ex. mohair"
           />
@@ -22,47 +22,7 @@ export default function YarnCompositionForm({ registerField }) {
           <input
             type="text"
             placeholder="ex. 77%"
-            {...registerField("composition.pourcentageone")}
-          />
-        </div>
-        <div className="FormSectionTitles">
-          <label>Material:</label>
-        </div>
-        <div className="FormInputText">
-          <input
-            {...registerField("composition.materialtwo")}
-            type="text"
-            placeholder="ex. wool"
-          />
-        </div>
-        <div className="FormSectionTitles">
-          <label>Pourcentage:</label>
-        </div>
-        <div className="FormInputText">
-          <input
-            type="text"
-            placeholder="ex. 18%"
-            {...registerField("composition.pourcentagetwo")}
-          />
-        </div>
-        <div className="FormSectionTitles">
-          <label>Material:</label>
-        </div>
-        <div className="FormInputText">
-          <input
-            {...registerField("composition.materialthree")}
-            type="text"
-            placeholder="ex. polyamid"
-          />
-        </div>
-        <div className="FormSectionTitles">
-          <label>Pourcentage:</label>
-        </div>
-        <div className="FormInputText">
-          <input
-            type="text"
-            placeholder="ex. 5%"
-            {...registerField("composition.pourcentagethree")}
+            {...registerField("composition.pourcentage")}
           />
         </div>
       </div>
